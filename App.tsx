3bfb4aa2a35a0d8e81490e40fb1ff2f481d9f1e7
Carrasco Romel <romelx23@gmail.com>
2021-11-04 15:19:25 -0500

    version 0.0.1

--- App.tsx
+++ App.tsx
@@ -14,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
