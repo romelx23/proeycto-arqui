55e6e0ad7b49687da55f12847f3b18df58a90a6a
CritianChipana <cristianchipanahuaman@gmail.com>
2021-11-29 11:34:34 -0500

    react-router-native agergado

--- App.tsx
+++ App.tsx
@@ -3,14 +3,34 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/Home/HomeScreen';
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from './src/screens/Login/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+// isSignedIn ? (
+//   <>
+//     <Stack.Screen name="Home" component={HomeScreen} />
+//     <Stack.Screen name="Profile" component={ProfileScreen} />
+//     <Stack.Screen name="Settings" component={SettingsScreen} />
+//   </>
+// ) : (
+//   <>
+//     <Stack.Screen name="SignIn" component={SignInScreen} />
+//     <Stack.Screen name="SignUp" component={SignUpScreen} />
+//   </>
+// )
+
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={HomeScreen} ></Stack.Screen>
+        <Stack.Screen name="login" component={LoginScreen} ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
 
-    <HomeScreen></HomeScreen>
-   /*  <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View> */
   );
 }
 
